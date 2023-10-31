f80835736526ffe013a74e558c9dfb0b229469b2
mwayanta <136800831+mwayanta@users.noreply.github.com>
2023-10-31 08:10:33 +0700

    Update wayan.sh

--- wayan.sh
+++ wayan.sh
@@ -1,9 +1,3 @@
-apt-get update
-apt-get install xterm -y
-apt-get install sudo
-adduser test
-usermod -aG sudo test
-apt-get install fluxbox -y
 clear
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 apt install ./chrome-remote-desktop_current_amd64.deb
