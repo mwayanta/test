7866f240aae155a9e9fe2b02d3784c0d6e99c325
mwayanta <136800831+mwayanta@users.noreply.github.com>
2023-12-03 07:41:54 +0700

    Add files via upload

---
+++ ngrok.sh
@@ -0,0 +1,1 @@
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.zip && unzip ngrok-v3-stable-linux-amd64.zip && ./ngrok config add-authtoken 1lzpsFqpjyd50bu443avWew6q6u_612SZ19XczkhtrvQ6HGcD && ./ngrok http 8080
