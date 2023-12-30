e58ff131b7fa11cf49d45eaba349537cdf43d422
mwayanta <136800831+mwayanta@users.noreply.github.com>
2023-12-30 21:33:36 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,4 +1,4 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.zip
 unzip ngrok-v3-stable-linux-amd64.zip
 ./ngrok config add-authtoken 1lzpsFqpjyd50bu443avWew6q6u_612SZ19XczkhtrvQ6HGcD
-./ngrok http 8181
+./ngrok http 6080
